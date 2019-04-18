5187484c9627a0d54c374d913b8ffc1e3c89ed8e
Patrick Hulce <patrick.hulce@gmail.com>
2019-04-18 15:48:19 -0500

    docs(www): add basic gatsby starter

---
+++ www/src/components/layout.js
@@ -0,0 +1,55 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import {StaticQuery, graphql} from 'gatsby'
+
+import Sidebar from './sidebar'
+import './layout.css'
+
+const Layout = ({children}) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div className="root-layout">
+          <Sidebar siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </div>
+      )}
+    />
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
