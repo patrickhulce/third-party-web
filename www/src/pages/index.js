461f550ae4ecde2dcdaf88b5b8a283b40bc51197
Patrick Hulce <patrick.hulce@gmail.com>
2019-04-18 16:11:49 -0500

    docs(www): add placeholder data visualzation component

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -2,13 +2,12 @@ import React from 'react'
 import {Link} from 'gatsby'
 
 import SEO from '../components/seo'
+import DataVisualization from '../components/data-visualization'
 
 const IndexPage = () => (
   <>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <DataVisualization />
     <Link to="/page-2/">Go to page 2</Link>
   </>
 )
