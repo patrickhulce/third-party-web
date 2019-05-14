18ef2fbfd847cbf663d02b050ce2cd98c49814ac
Patrick Hulce <patrick.hulce@gmail.com>
2019-05-13 21:48:03 -0500

    feat(data): add april data

--- sql/bootup-time-scripting.partial.sql
+++ sql/bootup-time-scripting.partial.sql
@@ -6,7 +6,7 @@ SELECT
   JSON_EXTRACT(report, '$.audits.bootup-time.details.items[<%= i %>].url') AS url,
   SAFE_CAST(JSON_EXTRACT(report, '$.audits.bootup-time.details.items[<%= i %>].scripting') AS FLOAT64) as executionTime
 FROM
-  `httparchive.lighthouse.2019_04_01_mobile`
+  `httparchive.lighthouse.2019_03_01_mobile`
 JOIN
   `lighthouse-infrastructure.third_party_web.2019_05`
 ON NET.HOST(JSON_EXTRACT(report, '$.audits.bootup-time.details.items[<%= i %>].url')) = domain
