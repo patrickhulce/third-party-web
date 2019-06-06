4b057fd092b935fe323ce122152cdaaaa5551138
Patrick Hulce <patrick.hulce@gmail.com>
2019-06-06 17:04:15 -0500

    fix: expand canonical domain set to all observed

--- bin/generate-canonical-domain-csv.js
+++ bin/generate-canonical-domain-csv.js
@@ -2,11 +2,21 @@ const _ = require('lodash')
 const fs = require('fs')
 const path = require('path')
 
-const entities = require('../dist/entities.json')
+const {getEntity} = require('../lib/')
+const observedDomainsDataset = require('../data/2019-04-01-origin-scripting.json')
 
-const entries = _.flatten(
-  entities.map(entity => entity.domains.map(domain => [domain, entity.domains[0]]))
-)
+const observedDomains = observedDomainsDataset.map(e => e.origin).filter(Boolean)
+
+const entries = observedDomains
+  .map(domain => {
+    const entity = getEntity(domain)
+    if (!entity) {
+      return undefined
+    }
+
+    return [domain, entity.domains[0]]
+  })
+  .filter(Boolean)
 
 fs.writeFileSync(
   path.join(__dirname, '../dist/domain-map.csv'),
