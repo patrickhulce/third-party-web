d870e6be857fb90b6a2402e79fb6a9dfe5930ad9
Patrick Hulce <patrick.hulce@gmail.com>
2019-07-25 11:08:38 -0500

    feat: add July data

--- sql/bootup-time-scripting.partial.sql
+++ sql/bootup-time-scripting.partial.sql
@@ -6,8 +6,8 @@ SELECT
   JSON_EXTRACT(report, '$.audits.bootup-time.details.items[<%= i %>].url') AS url,
   SAFE_CAST(JSON_EXTRACT(report, '$.audits.bootup-time.details.items[<%= i %>].scripting') AS FLOAT64) as executionTime
 FROM
-  `httparchive.lighthouse.2019_05_01_mobile`
+  `httparchive.lighthouse.2019_07_01_mobile`
 LEFT JOIN
-  `lighthouse-infrastructure.third_party_web.2019_06_06`
+  `lighthouse-infrastructure.third_party_web.2019_07_01`
 ON NET.HOST(JSON_EXTRACT(report, '$.audits.bootup-time.details.items[<%= i %>].url')) = domain
 <%= where %>
