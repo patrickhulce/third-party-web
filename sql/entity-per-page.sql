b7c7eff5805434b75235b89205f33203b1ac7fbb
Christophe Billiard <christophe.billiard@gmail.com>
2021-08-02 20:02:30 +0200

    refacto: [#142] Rewrite generated queries for cost and time reasons

--- sql/entity-per-page.sql
+++ sql/entity-per-page.sql
@@ -12,7 +12,29 @@ FROM (
     SUM(executionTime) AS totalExecutionTime,
     COUNT(executionTime) AS totalOccurrences,
     AVG(executionTime) AS averageExecutionTime
-  FROM <%= from_statement %>
+  FROM (
+    SELECT
+  domain,
+  canonicalDomain,
+  category,
+  JSON_VALUE(report, '$.requestedUrl') AS pageUrl,
+  NET.HOST(JSON_VALUE(bootupTimeItems, '$.url')) AS observedDomain,
+  JSON_VALUE(bootupTimeItems, '$.url') AS scriptUrl,
+  SAFE_CAST(JSON_VALUE(bootupTimeItems,
+      "$.scripting") AS FLOAT64) AS executionTime
+FROM (
+  SELECT
+    url AS page,
+    report
+  FROM
+    `httparchive.lighthouse.2020_05_01_mobile`
+  ),
+  UNNEST(JSON_QUERY_ARRAY(report,
+      '$.audits.bootup-time.details.items')) AS bootupTimeItems
+  INNER JOIN
+    `lighthouse-infrastructure.third_party_web.2020_05_01`
+  ON NET.HOST(JSON_VALUE(bootupTimeItems, "$.url")) = domain
+  )
   WHERE canonicalDomain IS NOT NULL
   GROUP BY pageUrl, canonicalDomain
 )
