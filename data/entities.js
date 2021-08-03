3157d3bdd7ef50a5af774334cd8ebfa7484213bd
Daniel Bachhuber <daniel@handbuilt.co>
2021-08-03 05:41:26 -0700

    Add intercom-facade pure JavaScript library

--- data/entities.js
+++ data/entities.js
@@ -1498,6 +1498,10 @@ module.exports = [
             name: 'React Live Chat Loader',
             repo: 'https://github.com/calibreapp/react-live-chat-loader',
           },
+          {
+            name: 'Intercom Facade',
+            repo: 'https://github.com/danielbachhuber/intercom-facade/',
+          },
         ],
       },
     ],
