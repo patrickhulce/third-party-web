8b1c7c3cf1947d538323c67e6c09a2bd7daa45a7
Barry Pollard <barry_pollard@hotmail.com>
2021-10-24 22:52:12 +0100

    Add static.cloudflareinsights.com

--- data/entities.js
+++ data/entities.js
@@ -357,7 +357,7 @@ module.exports = [
     name: 'Cloudflare',
     homepage: 'https://www.cloudflare.com/website-optimization/',
     categories: ['utility'],
-    domains: ['ajax.cloudflare.com', '*.nel.cloudflare.com'],
+    domains: ['ajax.cloudflare.com', '*.nel.cloudflare.com', 'static.cloudflareinsights.com'],
   },
   {
     name: 'WordPress',
