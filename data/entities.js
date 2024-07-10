d2fe72d696c79347612cd3daa7d20b8ab11c19cd
Romain GUILLAUME <Kporal@users.noreply.github.com>
2024-07-10 11:22:00 +0200

    feat(entities): update AB Tasty and Kameleoon (#1)
    * feat(entities): update AB Tasty and Kameleoon
    
    * feat: add kameleoon url examples
    
    * feat: add abtasty url examples
    
    * Update data/entities.js
    
    ---------
    
    Co-authored-by: Romain GUILLAUME <romain.guillaume@abtasty.com>
    Co-authored-by: Guillaume NICOLAS <guillaume.nicolas@abtasty.com>

--- data/entities.js
+++ data/entities.js
@@ -1708,8 +1708,10 @@ module.exports = [
   },
   {
     name: 'AB Tasty',
+    homepage: 'https://www.abtasty.com/',
     category: 'analytics',
     domains: ['*.abtasty.com', 'd1447tq2m68ekg.cloudfront.net'],
+    examples: ['try.abtasty.com']
   },
   {
     name: 'ABA RESEARCH',
@@ -5842,8 +5844,10 @@ module.exports = [
   },
   {
     name: 'Kameleoon',
+    homepage: 'https://www.kameleoon.com/',
     category: 'analytics',
-    domains: ['*.kameleoon.com', '*.kameleoon.eu'],
+    domains: ['*.kameleoon.com', '*.kameleoon.eu', '*.kameleoon.io'],
+    examples: ['data.kameleoon.io', 'kdm3fpv6il.kameleoon.eu'],
   },
   {
     name: 'Kampyle',
