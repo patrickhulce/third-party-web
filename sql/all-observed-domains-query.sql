fd22c61aec846a5c2d015bd66b2dd86fda2bdcaa
Guillaume NICOLAS <guin56@gmail.com>
2024-08-07 15:41:50 +0200

    fix(2024-06-01): use registered domain for third parties to count sub-domains (#6)
    * fix: get observed domains by registered domains and clean queries
    
    * fix: use registered domain for third parties to count sub-domains
    
    ---------
    
    Co-authored-by: Guillaume NICOLAS <guillaume.nicolas@abtasty.com>

--- sql/all-observed-domains-query.sql
+++ sql/all-observed-domains-query.sql
@@ -1,20 +1,9 @@
-SELECT
-  domain,
-  COUNT(0) AS totalOccurrences
+SELECT domain, COUNT(*) as totalOccurrences
 FROM (
-  SELECT
-    page,
-    NET.HOST(url) AS domain,
-    COUNT(0) AS totalOccurrences
-  FROM
-    `httparchive.requests.2022_01_01_mobile`
-  GROUP BY
-    page,
-    domain
+  SELECT page, NET.REG_DOMAIN(url) AS domain,
+  FROM `httparchive.requests.2024_06_01_mobile`
+  GROUP BY page, domain
 )
-GROUP BY
-  domain
-HAVING
-  totalOccurrences >= 50
-ORDER BY
-  totalOccurrences DESC
+GROUP BY domain
+HAVING totalOccurrences >= 50
+ORDER BY totalOccurrences DESC
