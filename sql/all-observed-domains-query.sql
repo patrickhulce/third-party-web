eb07e117805f22f9d90e32196a371230da4d3517
Guillaume NICOLAS <guillaume.nicolas@abtasty.com>
2024-08-08 12:33:57 +0200

    feat: keep observed domains mapped to entities requested on at least 50 different pages and generate 2024_07 data
    feat: keep saving in file all observed domains with minimum observations
    
    fix: tpw table exists
    
    feat: automated script splitting into 3 steps, add logs and clean table if needed
    
    feat: compute data for 2024_07_01

--- sql/all-observed-domains-query.sql
+++ sql/all-observed-domains-query.sql
@@ -1,20 +1,61 @@
+CREATE TEMP TABLE
+    entities (name STRING, domain STRING) AS
 SELECT
-  domain,
-  COUNT(0) AS totalOccurrences
-FROM (
-  SELECT
-    page,
-    NET.HOST(url) AS domain,
-    COUNT(0) AS totalOccurrences
-  FROM
-    `httparchive.requests.2022_01_01_mobile`
-  GROUP BY
-    page,
-    domain
-)
-GROUP BY
-  domain
-HAVING
-  totalOccurrences >= 50
+    JSON_VALUE(entity, '$.name') AS name,
+    JSON_VALUE(entity, '$.domain') as domain
+FROM
+    UNNEST (JSON_QUERY_ARRAY(@entities_string, '$')) AS entity;
+
+-- Map each observed domain and count to entity
+CREATE TEMP TABLE
+    entity_domain_count (domain STRING, totalOccurrences INT, name STRING) AS
+SELECT
+    domain_occurrences.domain as domain,
+    domain_occurrences.totalOccurrences as totalOccurrences,
+    entities.name as name,
+FROM
+    (
+        -- How many times an observed domain is called in archive
+        SELECT
+            domain,
+            COUNT(0) AS totalOccurrences
+        FROM
+            (
+                SELECT
+                    page,
+                    NET.HOST(url) AS domain
+                FROM
+                    `httparchive.requests.2022_01_01_mobile`
+                GROUP BY
+                    page,
+                    domain
+            )
+        GROUP BY
+            domain
+    ) as domain_occurrences
+    JOIN
+    -- Mapping between a domain and an entity
+    entities ON domain_occurrences.domain LIKE REPLACE(entities.domain, '*', '%');
+
+-- Get entities with at least 50 observed domains
+WITH
+    entity_count AS (
+        SELECT
+            name,
+            SUM(totalOccurrences) as totalOccurrences
+        FROM
+            entity_domain_count
+        GROUP BY
+            name
+        HAVING
+            totalOccurrences >= 50
+    )
+    -- Get observed domains owned by entities with at least 50 observed domains
+SELECT
+    entity_domain_count.domain,
+    entity_domain_count.totalOccurrences
+FROM
+    entity_domain_count
+    JOIN entity_count ON entity_count.name = entity_domain_count.name
 ORDER BY
-  totalOccurrences DESC
+    totalOccurrences DESC
--- sql/entity-per-page.sql
+++ sql/entity-per-page.sql
@@ -5,39 +5,45 @@ SELECT
   SUM(totalOccurrences) AS totalScripts,
   AVG(totalExecutionTime) AS averageExecutionTime,
   AVG(averageExecutionTime) AS averageScriptExecutionTime
-FROM (
-  SELECT
-    pageUrl,
-    canonicalDomain,
-    SUM(executionTime) AS totalExecutionTime,
-    COUNT(executionTime) AS totalOccurrences,
-    AVG(executionTime) AS averageExecutionTime
-  FROM (
+FROM
+  (
     SELECT
-  domain,
-  canonicalDomain,
-  category,
-  JSON_VALUE(report, '$.requestedUrl') AS pageUrl,
-  NET.HOST(JSON_VALUE(bootupTimeItems, '$.url')) AS observedDomain,
-  JSON_VALUE(bootupTimeItems, '$.url') AS scriptUrl,
-  SAFE_CAST(JSON_VALUE(bootupTimeItems,
-      "$.scripting") AS FLOAT64) AS executionTime
-FROM (
-  SELECT
-    url AS page,
-    report
-  FROM
-    `httparchive.lighthouse.2022_01_01_mobile`
-  ),
-  UNNEST(JSON_QUERY_ARRAY(report,
-      '$.audits.bootup-time.details.items')) AS bootupTimeItems
-  INNER JOIN
-    `lighthouse-infrastructure.third_party_web.2022_01_01`
-  ON NET.HOST(JSON_VALUE(bootupTimeItems, "$.url")) = domain
+      pageUrl,
+      canonicalDomain,
+      SUM(executionTime) AS totalExecutionTime,
+      COUNT(executionTime) AS totalOccurrences,
+      AVG(executionTime) AS averageExecutionTime
+    FROM
+      (
+        SELECT
+          domain,
+          canonicalDomain,
+          category,
+          JSON_VALUE(report, '$.requestedUrl') AS pageUrl,
+          NET.HOST(JSON_VALUE(bootupTimeItems, '$.url')) AS observedDomain,
+          JSON_VALUE(bootupTimeItems, '$.url') AS scriptUrl,
+          SAFE_CAST(
+            JSON_VALUE(bootupTimeItems, "$.scripting") AS FLOAT64
+          ) AS executionTime
+        FROM
+          (
+            SELECT
+              url AS page,
+              report
+            FROM
+              `httparchive.lighthouse.2022_01_01_mobile`
+          ),
+          UNNEST (
+            JSON_QUERY_ARRAY(report, '$.audits.bootup-time.details.items')
+          ) AS bootupTimeItems
+          INNER JOIN `lighthouse-infrastructure.third_party_web.2022_01_01` ON NET.HOST(JSON_VALUE(bootupTimeItems, "$.url")) = domain
+      )
+    WHERE
+      canonicalDomain IS NOT NULL
+    GROUP BY
+      pageUrl,
+      canonicalDomain
   )
-  WHERE canonicalDomain IS NOT NULL
-  GROUP BY pageUrl, canonicalDomain
-)
 GROUP BY
   canonicalDomain
 ORDER BY
